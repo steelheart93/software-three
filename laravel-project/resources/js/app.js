dabbf370adcf78a308384e3f3070563e01c9d233
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2021-11-12 00:31:17 -0500

    require('@ckeditor/ckeditor5...'); >npm run dev

--- laravel-project/resources/js/app.js
+++ laravel-project/resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+const ClassicEditor = require('@ckeditor/ckeditor5-build-classic');
+ClassicEditor
+    .create(document.querySelector('#content_publication'))
+    .then(editor => {
+        console.log(editor);
+    })
+    .catch(error => {
+        console.error(error);
+    });
