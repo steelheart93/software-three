8076db056ded11704c6e73dd22c98a02d6384c92
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2021-11-28 00:05:58 -0500

    primer commit last-project

---
+++ last-project/resources/views/coronapp/about.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+@section('content')
+    <div class="jumbotron text-center">
+        <h1 class="display-4">InfoCOVID</h1>
+        <p class="lead">Proyecto final para la clase de Software 3.</p>
+        <hr class="my-4">
+        <p>
+            Copyright (c) 2021 InfoCOVID Team, All Rights Reserved.
+        </p>
+    </div>
+@endsection
