41f3c69ac7f387dd48ee3f175848f90580942aee
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2021-12-13 22:02:33 -0500

    no olvides que existe type="datetime"

--- last-project/webpack.mix.js
+++ last-project/webpack.mix.js
@@ -11,24 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.autoload({
-    'jquery': ['jQuery', '$']
-});
-
-mix.styles([
-    'node_modules/bootstrap/dist/css/bootstrap.css',
-    'node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css',
-    'resources/css/app.css'
-], 'public/css/app.css');
-
-mix.js([
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/popper.js/dist/popper.js',
-    'node_modules/bootstrap/dist/js/bootstrap.js',
-    'node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js',
-    'resources/js/app.js'
-], 'public/js/app.js');
-
-mix.vue();
-
-mix.sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
