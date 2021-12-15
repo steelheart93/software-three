f3d3079f7f682cea29cd3f3d843ac357e213ca66
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2021-12-14 21:48:48 -0500

    estados sin terminar

--- last-project/resources/views/coronapp/charts.blade.php
+++ last-project/resources/views/coronapp/charts.blade.php
@@ -2,17 +2,39 @@
 @section('content')
     <h1 class="pt-2 text-center">Gráficos con Google Charts</h1>
     <div class="row">
-        <div class="col-md-6 p-4">
-            <!--google-chart [title]="pieChart.title" [type]="pieChart.type" [columns]="pieChart.columnNames"
-                    [data]="pieChart.data" [options]="pieChart.options"></google-chart-->
-            {{ $estados }}
+        <div class="col-md-12 p-4">
+            <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
         </div>
-        <div class="col-md-6 p-4">
-            <!--google-chart [title]="columnChart.title" [type]="columnChart.type" [columns]="columnChart.columnNames"
-                    [data]="columnChart.data"></google-chart-->
+
+        <div class="col-md-12 p-4">
             {{ $departamentos }}
         </div>
     </div>
 
-    <script></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load("current", {
+            packages: ["corechart"]
+        });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Estado', 'Cantidad'],
+                ['Work', 11],
+                ['Eat', 2],
+                ['Commute', 2],
+                ['Watch TV', 2],
+                ['Sleep', 7]
+            ]);
+
+            var options = {
+                title: 'My Daily Activities',
+                is3D: true,
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+            chart.draw(data, options);
+        }
+    </script>
 @endsection
